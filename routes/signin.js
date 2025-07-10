2fa186fed82f9174688fcfad41906b086b446d79
j2223 <j2223@matsue-ct.ac.jp>
2025-07-10 05:41:11 +0000

    Chater17途中

--- routes/signin.js
+++ routes/signin.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../knex");
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   res.render("signin", {
     title: "Sign in",
     isAuth: isAuth,
@@ -13,36 +14,31 @@ const isAuth = Boolean(userId);
 
 router.post('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
 
   knex("users")
     .where({
       name: username,
-      password: password,
     })
     .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          isAuth: isAuth,
-          errorMessage: ["ユーザが見つかりません"],
-        });
-      } else {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("signin", {
-        title: "Sign in",
-        isAuth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
+   .then(function(results) {
+  const comparedPassword = bcrypt.compare(password, results[0].password);
+  if (results.length === 0) {
+    res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
     });
+  } else {
+    req.session.regenerate((err) => {
+      req.session.userid = results[0].id;
+      req.session.username = results[0].name;
+      res.redirect('/');
+    });
+  }
+})
 });
 
 module.exports = router;
